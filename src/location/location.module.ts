4948e2b64ac3665da6fd0e282cbe418ce0e85ebe
hdwix <handoko.darmawan@gmail.com>
2025-10-16 23:28:46 +0700

    [handoko] edit config for bull-module

--- src/location/location.module.ts
+++ src/location/location.module.ts
@@ -5,11 +5,17 @@ import { LocationService } from './domain/services/location.service';
 import { GeolocationRepository } from './domain/services/geolocation.repository';
 import { LocationProcessor } from './domain/processors/location.processor';
 import { LOCATION_QUEUE_NAME } from './domain/services/location.types';
+import { BullBoardModule } from '@bull-board/nestjs';
+import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
 
 @Module({
   imports: [
     ConfigModule,
     BullModule.registerQueue({ name: LOCATION_QUEUE_NAME }),
+    BullBoardModule.forFeature({
+      name: LOCATION_QUEUE_NAME,
+      adapter: BullMQAdapter,
+    }),
   ],
   providers: [LocationService, GeolocationRepository, LocationProcessor],
   exports: [LocationService],
