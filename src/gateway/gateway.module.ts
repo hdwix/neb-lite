3427403471aa307b098ab3d227704529f9556260
hdwix <163229395+hdwix@users.noreply.github.com>
2025-10-24 13:34:53 +0700

    Fix queue injection for authentication service

--- src/gateway/gateway.module.ts
+++ src/gateway/gateway.module.ts
@@ -1,21 +1,6 @@
 import { Module } from '@nestjs/common';
 import { GatewayController } from './app/controllers/gateway.controller';
 import { IamModule } from '../iam/iam.module';
-import { AuthenticationService } from '../iam/domain/services/authentication.service';
-import { HashingService } from '../iam/domain/services/hashing.service';
-import { BcryptService } from '../iam/domain/services/bcrypt.service';
-import { APP_GUARD } from '@nestjs/core';
-import { AccessRoleGuard } from '../iam/app/guards/access-role/access-role.guard';
-import { AccessTokenGuard } from '../iam/app/guards/access-token/access-token.guard';
-import { AuthenticationGuard } from '../iam/app/guards/authentication/authentication.guard';
-import { JwtModule, JwtService } from '@nestjs/jwt';
-import { ConfigModule } from '@nestjs/config';
-import jwtConfig from '../iam/app/config/jwt.config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { RiderProfile } from '../iam/domain/entities/rider-profile.entity';
-import { DriverProfile } from '../iam/domain/entities/driver-profile.entity';
-import { RiderProfileRepository } from '../iam/infrastructure/repository/rider-profile.repository';
-import { DriverProfileRepository } from '../iam/infrastructure/repository/driver-profile.repository';
 import { DatabaseModule } from '../infrastructure/modules/database.module';
 import { LocationModule } from '../location/location.module';
 import { RidesModule } from '../rides/rides.module';
@@ -28,29 +13,7 @@ import { NotificationsModule } from '../notifications/notifications.module';
     LocationModule,
     RidesModule,
     NotificationsModule,
-    JwtModule.registerAsync(jwtConfig.asProvider()),
-    ConfigModule.forFeature(jwtConfig),
-    TypeOrmModule.forFeature([RiderProfile, DriverProfile]),
   ],
   controllers: [GatewayController],
-  providers: [
-    AuthenticationService,
-    {
-      provide: HashingService,
-      useClass: BcryptService,
-    },
-    {
-      provide: APP_GUARD,
-      useClass: AuthenticationGuard,
-    },
-    {
-      provide: APP_GUARD,
-      useClass: AccessRoleGuard,
-    },
-    AccessTokenGuard,
-    JwtService,
-    RiderProfileRepository,
-    DriverProfileRepository,
-  ],
 })
 export class GatewayModule {}
