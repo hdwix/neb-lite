93e8a9552b73eace1798329b4108026b50be1c6a
hdwix <handoko.darmawan@gmail.com>
2025-11-09 12:20:05 +0700

    [handoko] set db config connection for rds

--- src/infrastructure/modules/database.module.ts
+++ src/infrastructure/modules/database.module.ts
@@ -3,32 +3,44 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RiderProfile } from '../../iam/domain/entities/rider-profile.entity';
 import { DriverProfile } from '../../iam/domain/entities/driver-profile.entity';
-import fs from 'fs';
+import axios from 'axios';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('POSTGRES_HOST'),
-        port: configService.get<number>('POSTGRES_PORT'),
-        username: configService.get<string>('POSTGRES_USER'),
-        password: configService.get<string>('POSTGRES_PASSWORD'),
-        database: configService.get<string>('POSTGRES_DB'),
-        migrations: [__dirname + '/../migrations/*{.ts,.js}'],
-        dateStrings: true,
-        logging: false,
-        autoLoadEntities: true,
-        synchronize: configService.get<boolean>('DB_SYNCHRONIZE') || false,
-        entities: [RiderProfile, DriverProfile],
-        ssl: {
-          require: true,
-          rejectUnauthorized: true,
-          ca: fs.readFileSync('./ca/global-bundle.pem').toString(),
-        },
-      }),
+      useFactory: async (configService: ConfigService) => {
+        const isLocal = configService.get<string>('NODE_ENV') === 'local';
+
+        let sslConfig;
+        if (!isLocal) {
+          // Fetch AWS RDS global CA bundle dynamically
+          const caResponse = await axios.get(
+            'https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem',
+          );
+          sslConfig = {
+            ca: caResponse.data,
+          };
+        } else {
+          sslConfig = false;
+        }
+
+        return {
+          type: 'postgres',
+          host: configService.get<string>('POSTGRES_HOST'),
+          port: configService.get<number>('POSTGRES_PORT'),
+          username: configService.get<string>('POSTGRES_USER'),
+          password: configService.get<string>('POSTGRES_PASSWORD'),
+          database: configService.get<string>('POSTGRES_DB'),
+          entities: [RiderProfile, DriverProfile],
+          migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+          autoLoadEntities: true,
+          synchronize: true,
+          logging: false,
+          ssl: sslConfig, // false for local, CA for non-local
+        };
+      },
     }),
   ],
   exports: [TypeOrmModule],
